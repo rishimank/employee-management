c1ee4822b69fa6b1cfc4e0c03fbebe4d9a2bba0a
Rishi Mankala <rishimankala123@gmail.com>
2026-01-07 16:12:34 -0500

    Added EmployeeRepository.java under the repository package. Used repository annotation to mark the interface as a Spring Bean, and extended JpaRespotiroy to allow for all common databases to be injected into the Employee entity

---
+++ src/main/java/dev/rishi/ems_backend/repository/EmployeeRepository.java
@@ -0,0 +1,14 @@
+// an interface that extends Spring Data JPA's JpaRepository
+
+package dev.rishi.ems_backend.repository;
+
+import dev.rishi.ems_backend.entity.Employee;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// annotation marks the interface as a Spring Bean so that it can be injected into other layers (like Service layer)
+@Repository
+// SD JPA automatically provides all the common databases for the Employee entity
+public interface EmployeeRepository extends JpaRepository<Employee, Long> {
+
+}

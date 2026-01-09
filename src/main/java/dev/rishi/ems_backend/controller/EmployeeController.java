5c85a5d2fcff49bbe28d410d681ca06cc353b33a
Rishi Mankala <rishimankala123@gmail.com>
2026-01-08 22:10:41 -0500

    Added comments to EmployeeController.java

--- src/main/java/dev/rishi/ems_backend/controller/EmployeeController.java
+++ src/main/java/dev/rishi/ems_backend/controller/EmployeeController.java
@@ -1,3 +1,12 @@
+// public entry point to the backend
+// controller between HTTP (JSON, URLs, verbs) and Java
+
+// what it does:
+// 1. converts HTTP requests to Java objects
+// 2. routes URLs to methods
+// 3. calls business logic
+// 4. converts java objects to HTTP responses
+
 package dev.rishi.ems_backend.controller;
 
 import dev.rishi.ems_backend.entity.Employee;
@@ -17,6 +26,7 @@ public class EmployeeController {
     private EmployeeService employeeService;
 
     // CREATE employee
+    // below annotation handles HTTP POST (POST /api/employees)
     @PostMapping
     public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
         Employee savedEmployee = employeeService.saveEmployee(employee);
@@ -24,12 +34,14 @@ public class EmployeeController {
     }
 
     // GET all employees
+    // handles HTTP GET
     @GetMapping
     public ResponseEntity<List<Employee>> getAllEmployees() {
         return ResponseEntity.ok(employeeService.getAllEmployees());
     }
 
     // GET employee by ID
+    // handles HTTP DELETE
     @GetMapping("/{id}")
     public ResponseEntity<Employee> getEmployeeById(@PathVariable Long id) {
         Optional<Employee> employee = employeeService.getEmployeeById(id);
@@ -40,9 +52,10 @@ public class EmployeeController {
     }
 
     // DELETE employee
+    // the base URL
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteEmployee(@PathVariable Long id) {
         employeeService.deleteEmployee(id);
-        return ResponseEntity.noContent().build();
+        return ResponseEntity.noContent().build(); // converts Java objects to HTTP responses
     }
 }
